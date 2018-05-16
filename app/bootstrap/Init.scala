cef17e965faa302dbe4b162441962abf00b9f1ad
kondaaditya <adityaprasad.k@gmail.com>
2018-05-16 19:56:32 -0400

    Initial commit

---
+++ app/bootstrap/Init.scala
@@ -0,0 +1,37 @@
+package bootstrap
+
+import org.apache.spark.sql.SparkSession
+import play.api._
+
+object Init extends GlobalSettings {
+
+  var sparkSession: SparkSession = _
+
+  /**
+   * On start load the json data from conf/data.json into in-memory Spark
+   */
+  override def onStart(app: Application) {
+
+    // Spark Conf & hive context
+    sparkSession = SparkSession.builder
+      .master("local")
+      .appName("ApplicationController")
+      .getOrCreate()
+
+    val dataFrame = sparkSession.read.json("conf/data.json")
+    dataFrame.createOrReplaceTempView("godzilla")
+  }
+
+  /**
+   * On stop clear the sparksession
+   */
+  override def onStop(app: Application) {
+    sparkSession.stop()
+  }
+
+  def getSparkSessionInstance = {
+    sparkSession
+  }
+}
+
+
